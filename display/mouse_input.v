f8576426faa10f10730bcd444d239da03e82bfe1
瘋狂挺腰獸 <s111062109@m111.nthu.edu.tw>
2023-12-23 11:24:33 +0800

    Update mouse_input.v

--- display/mouse_input.v
+++ display/mouse_input.v
@@ -13,8 +13,8 @@ module mouse_input(
     reg [9:0] end_mouse_x, end_mouse_y, next_end_mouse_x, next_end_mouse_y;
     reg [9:0] delta_x, delta_y, next_delta_x, next_delta_y;
     wire [9:0] abs_delta_x, abs_delta_y;
-    assign abs_delta_x = delta_x > 0 ? delta_x : -delta_x;
-    assign abs_delta_y = delta_y > 0 ? delta_y : -delta_y;
+    assign abs_delta_x = delta_x < 0 ? -delta_x : delta_x;
+    assign abs_delta_y = delta_y < 0 ? -delta_y : delta_y;
     wire x_is_larger;
     reg done;
     always @ (posedge clk) begin
@@ -42,14 +42,15 @@ module mouse_input(
     assign write_enable = MOUSE_LEFT || MOUSE_RIGHT;
     assign write_data = MOUSE_LEFT;
     assign x_is_larger = (abs_delta_x > abs_delta_y) ? 1 : 0;
+    wire _start = (MOUSE_LEFT || MOUSE_RIGHT) && ( MOUSE_X_POS != end_mouse_x || MOUSE_Y_POS != end_mouse_y);
     always @(*) begin
         case(state)
             WAIT: begin
                 next_delta_x = MOUSE_X_POS - end_mouse_x;
                 next_delta_y = MOUSE_Y_POS - end_mouse_y;
-                state_next = ((MOUSE_LEFT || MOUSE_RIGHT) && ( MOUSE_X_POS != end_mouse_x || MOUSE_Y_POS != end_mouse_y)) ? WRITE1 : WAIT;
-                next_prev_mouse_x = ((MOUSE_LEFT || MOUSE_RIGHT) && (MOUSE_X_POS != end_mouse_x || MOUSE_Y_POS != end_mouse_y)) ? next_prev_mouse_x : MOUSE_X_POS;
-                next_prev_mouse_y =((MOUSE_LEFT || MOUSE_RIGHT) && (MOUSE_X_POS != end_mouse_x || MOUSE_Y_POS != end_mouse_y)) ? next_prev_mouse_y : MOUSE_Y_POS;
+                state_next = _start ? WRITE1 : WAIT;
+                next_prev_mouse_x = _start ? next_prev_mouse_x : MOUSE_X_POS;
+                next_prev_mouse_y =_start ? next_prev_mouse_y : MOUSE_Y_POS;
                 next_count = 0;
                 next_end_mouse_x = MOUSE_X_POS;
                 next_end_mouse_y = MOUSE_Y_POS;
@@ -65,7 +66,7 @@ module mouse_input(
                 next_count = count;
                 if(x_is_larger) begin
                     state_next = (count == abs_delta_x) ? DONE : WRITE2;
-                    write_addr = (prev_mouse_x + count) + (prev_mouse_y * count * delta_y / delta_x) * 640;
+                    write_addr = (prev_mouse_x + count) + (prev_mouse_y + count * delta_y / delta_x) * 640;
                 end
                 else begin
                     state_next = (count == abs_delta_y) ? DONE : WRITE2;
@@ -81,7 +82,7 @@ module mouse_input(
                 next_prev_mouse_y = prev_mouse_y;
                 next_count = count + 1;
                 state_next = WRITE1;  
-                write_addr = x_is_larger ? (prev_mouse_x + count) + (prev_mouse_y * count * delta_y / delta_x + 1) * 640 : (prev_mouse_x + count * delta_x / delta_y + 1) + (prev_mouse_y + count) * 640;
+                write_addr = x_is_larger ? (prev_mouse_x + count) + (prev_mouse_y + count * delta_y / delta_x + 1) * 640 : (prev_mouse_x + count * delta_x / delta_y + 1) + (prev_mouse_y + count) * 640;
             end
             DONE: begin
                 state_next = WAIT;
